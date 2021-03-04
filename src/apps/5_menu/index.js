68fd0efd6d0f4246b307bfcc3d1953a93d4228dd
Romenick Tester <romenick.tester@gmail.com>
2021-03-04 14:32:52 +0000

    menu app: manual approach | completed

--- src/apps/5_menu/index.js
+++ src/apps/5_menu/index.js
@@ -6,7 +6,35 @@ import "./source/index.css";
 
 function MenuApp() {
   const [menu, setMenu] = useState(items);
-  const [categories, setCategories] = useState([]);
+
+  const filterItems = (category) => {
+
+    switch (category) {
+      case "breakfast":
+        const breakfastItems = items.filter((item) => item.category === "breakfast");
+        setMenu(breakfastItems);
+        return;
+
+      case "lunch":
+        const lunchItems = items.filter((item) => item.category === "lunch");
+        setMenu(lunchItems);
+        return;
+
+      case "dinner":
+        const dinnerItems = items.filter((item) => item.category === "dinner");
+        setMenu(dinnerItems);
+        return;
+
+      case "shakes":
+        const shakesItems = items.filter((item) => item.category === "shakes");
+        setMenu(shakesItems);
+        return;
+
+      default:
+        setMenu(items);
+        return;
+    }
+  }
 
   return (
     <main>
@@ -15,7 +43,7 @@ function MenuApp() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
+        <Categories filterItems={filterItems} />
         <Menu items={menu} />
       </section>
       hello</main>
