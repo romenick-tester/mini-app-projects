6b8852c9b323cba725229bf2de8924287429fcb9
Romenick Tester <romenick.tester@gmail.com>
2021-03-30 17:26:57 +0100

    grocery bud -> clear items + remove item

--- src/10_grocery_bud/Alert.js
+++ src/10_grocery_bud/Alert.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 
-const Alert = ({ type, msg, removeAlert }) => {
+const Alert = ({ type, msg, removeAlert, list }) => {
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -9,7 +9,7 @@ const Alert = ({ type, msg, removeAlert }) => {
     return () => {
       clearTimeout(timeout);
     }
-  }, [])
+  }, [list]);
 
   return (
     <p className={`alert alert-${type}`}>{msg}</p>
--- src/10_grocery_bud/index.js
+++ src/10_grocery_bud/index.js
@@ -34,10 +34,21 @@ function GroceryBud() {
     setAlert({ msg, type, show });
   };
 
+  function clearList() {
+    showAlert("empty list", "danger", true);
+    setList([]);
+  }
+
+  function removeItem(id) {
+    showAlert("item removed!", "danger", true);
+    const filteredItems = list.filter((item) => item.id !== id);
+    setList(filteredItems);
+  }
+
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
         <h3>grocery bud</h3>
         <div className="form-control">
           <input
@@ -54,8 +65,8 @@ function GroceryBud() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} />
-          <button className="clear-btn">
+          <List items={list} removeItem={removeItem} />
+          <button className="clear-btn" onClick={clearList}>
             clear items
           </button>
         </div>
