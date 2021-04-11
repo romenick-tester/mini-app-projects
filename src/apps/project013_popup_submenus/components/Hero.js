a71fb3aae86e8f797aae5cbe43e001c9654a8f74
Romenick Tester <romenick.tester@gmail.com>
2021-04-11 11:51:31 +0100

    submenus -> hero component -> articles

--- src/apps/project013_popup_submenus/components/Hero.js
+++ src/apps/project013_popup_submenus/components/Hero.js
@@ -1,10 +1,27 @@
 import React from "react";
 import phoneImg from "../assets/images/phone.svg";
+import { useGlobalContext } from "../assets/context";
 
 const Hero = () => {
+  const { closeSubmenu } = useGlobalContext();
 
   return (
-    <h2>hero component</h2>
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>payment infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium dolor ipsam rerum, minus adipisci      commodi! Quibusdam cupiditate id vero corporis!
+          </p>
+          <button className="btn">
+            start now
+          </button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} alt="phone" className="phone-img" />
+        </article>
+      </div>
+    </section>
   )
 }
 
