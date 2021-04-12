52aacfc515db221bfefe1bb96550b5bc51c8570e
Romenick Tester <romenick.tester@gmail.com>
2021-04-12 23:35:52 +0100

    cocktails -> cocktail-list component -> cocktail component -> display cocktail details

--- src/apps/project015_cocktails/assets/context.js
+++ src/apps/project015_cocktails/assets/context.js
@@ -20,7 +20,6 @@ const AppProvider = ({ children }) => {
       if (drinks) {
         const newDrinks = drinks.map((item) => {
           const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = item;
-
           return { id: idDrink, name: strDrink, image: strDrinkThumb, info: strAlcoholic, glass: strGlass };
         });
 
