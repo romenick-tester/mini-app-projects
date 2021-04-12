daaae6822aa66932beeb8c26f87dbd57d7a576fe
Romenick Tester <romenick.tester@gmail.com>
2021-04-12 13:35:44 +0100

    cockails -> home page + error page -> layout setup

--- src/apps/project015_cocktails/pages/About.js
+++ src/apps/project015_cocktails/pages/About.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit odit placeat, facere odio ullam voluptates quidem nesciunt fuga ut magnam rem, magni sed, mollitia error. Fuga ad id dolorem veritatis.
+      </p>
+    </section>
   )
 }
 
