d857cebc2e0666d73da4b6c4f9b392082f9ca86e
Romenick Tester <romenick.tester@gmail.com>
2021-04-13 13:15:15 +0100

    markdown => initial setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Project1 from "./apps/completed/project015_cocktails"
-import Project2 from "./apps/project015_cocktails";
+import Project2 from "./apps/project016";
 
 function App() {
-    const isCompleted = true;
+    const isCompleted = false;
 
     return isCompleted ? <Project1 /> : <Project2 />;
 }
