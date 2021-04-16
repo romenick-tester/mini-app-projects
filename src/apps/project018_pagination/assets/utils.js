f417b9869a81f4432588c3e538ad470144dc9a3a
Romenick Tester <romenick.tester@gmail.com>
2021-04-16 23:10:12 +0100

    pagination => utils => paginate => paginate logic => divide array into separate arrays

--- src/apps/project018_pagination/assets/utils.js
+++ src/apps/project018_pagination/assets/utils.js
@@ -1,3 +1,16 @@
-const paginate = () => {}
+const paginate = (followers) => {
 
-export default paginate
+    const itemsPerPage = 9;
+
+    const pages = Math.ceil(followers.length / itemsPerPage);
+
+    const newFollowers = Array.from({ length: pages }, (_, index) => {
+        const start = index * itemsPerPage;
+
+        return followers.slice(start, start + itemsPerPage);
+    });
+
+    return newFollowers;
+}
+
+export default paginate;
