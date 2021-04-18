f7c83b6c3effc7c34407b660bc97c3e7e7a9a486
Romenick Tester <romenick.tester@gmail.com>
2021-04-18 23:16:23 +0100

    stock photos => fetch data logic + query input | project completed

--- src/apps/project019_stock_photos/index.js
+++ src/apps/project019_stock_photos/index.js
@@ -5,25 +5,40 @@ import Photo from "./Photo";
 import "./assets/index.css";
 
 const mainUrl = `https://api.unsplash.com/photos/`
-//const searchUrl = `https://api.unsplash.com/search/photos/`
+const searchUrl = `https://api.unsplash.com/search/photos/`
 const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState("");
 
   const fetchImages = useCallback(async () => {
     setLoading(true);
-    let urlPage = `&page=${page}`
-    let url = `${mainUrl}${clientID}${urlPage}`;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+    let url;
+
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
+
     try {
       const res = await fetch(url);
       const data = await res.json();
 
       if (data) {
         setPhotos((state) => {
-          return [...state, ...data]
+          if (query && page === 1) {
+            return data.results;
+          } else if (query) {
+            return [...state, ...data.results];
+          } else {
+            return [...state, ...data]
+          }
         });
       } else {
         setPhotos([]);
@@ -33,7 +48,7 @@ function App() {
       console.error(error.message);
       setLoading(false);
     }
-  }, [page])
+  }, [page, query])
 
   useEffect(() => {
     fetchImages();
@@ -51,17 +66,24 @@ function App() {
     return () => {
       window.removeEventListener("scroll", event);
     }
-  }, [loading, page]);
+  }, [loading]);
 
   function handleSubmit(e) {
     e.preventDefault();
+    setPage(1);
   }
 
   return (
     <main>
       <section className="search">
         <form className="search-form">
-          <input type="text" className="form-input" placeholder="search" />
+          <input
+            type="text"
+            className="form-input"
+            placeholder="search"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
           <button type="submit" className="submit-btn" onClick={handleSubmit} >
             <FaSearch />
           </button>
