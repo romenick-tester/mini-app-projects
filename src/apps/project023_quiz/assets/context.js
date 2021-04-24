01bf4f92a136762f3f1bc4192a1989637fe85bd9
Romenick Tester <romenick.tester@gmail.com>
2021-04-24 13:00:03 +0100

    quiz => construct api + context + fetch api

--- src/apps/project023_quiz/assets/context.js
+++ src/apps/project023_quiz/assets/context.js
@@ -10,11 +10,52 @@ const API_ENDPOINT = "https://opentdb.com/api.php?";
 
 const url = "";
 
+const tempURL = `${API_ENDPOINT}amount=10&category=21&difficulty=easy&type=multiple`;
+
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+  const [waiting, setWaiting] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [error, setError] = useState(false);
+  const [modal, setModal] = useState(false);
   
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>
+  const fetchQuestions = async function() {
+    setLoading(true);
+    setWaiting(false);
+
+    try {
+      const res = await fetch(tempURL);
+      const data = await res.json();
+
+      if(data && data.results.length > 0) {
+        setQuestions(data.results);
+        setError(false);
+        setLoading(false);
+        setWaiting(true);
+      } else {
+        setWaiting(true);
+        setError(true);
+      }
+    } catch (err) {
+      console.error(err.message);
+      setError(true);
+      setLoading(false);
+      setWaiting(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+  
+  const values = { waiting, loading, questions, index, correct, error, modal };
+  return (
+    <AppContext.Provider value={{...values}}>{children}</AppContext.Provider>
+  )
 };
 
 const useGlobalContext = () => {
--- src/apps/project023_quiz/index.js
+++ src/apps/project023_quiz/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useGlobalContext } from "./assets/context";
+import { AppProvider, useGlobalContext } from "./assets/context";
 import SetupForm from "./SetupForm";
 import Loading from "./Loading";
 import Modal from "./Modal";
@@ -8,7 +8,28 @@ import "./assets/index.css";
 
 function Quiz() {
   return (
-    <h2>quiz starter</h2>
+    <AppProvider>
+      <Index />
+    </AppProvider>
+    )
+};
+
+function Index() {
+  const { waiting, loading, questions, index, correct } = useGlobalContext();
+
+  if(waiting) {
+    return <SetupForm />
+  }
+
+  if(loading) {
+    return <Loading />
+  }
+
+  const { correct_answer, incorrect_answers, question } = questions[index];
+  return (
+    <>
+      <h2>quiz starter</h2>
+    </>
   )
 }
 
